68f187e36fcb635b4f97a0adc91f54582a4a3e2d
John Lindquist <johnlindquist@gmail.com>
2019-04-09 20:59:04 -0600

    Tue Apr  9 20:59:04 MDT 2019

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,63 +79,63 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-feed`,
-    //   options: {
-    //     query: `
-    //       {
-    //         site {
-    //           siteMetadata {
-    //             title
-    //             description
-    //             siteUrl
-    //             site_url: siteUrl
-    //           }
-    //         }
-    //       }
-    //     `,
-    //     feeds: [
-    //       {
-    //         serialize: ({ query: { site, allMdx } }) => {
-    //           return allMdx.edges.map(edge => {
-    //             return Object.assign({}, edge.node.frontmatter, {
-    //               description: edge.node.excerpt,
-    //               date: edge.node.fields.date,
-    //               url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-    //               guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-    //               custom_elements: [{ 'content:encoded': edge.node.html }],
-    //             })
-    //           })
-    //         },
-    //         query: `
-    //           {
-    //             allMdx(
-    //               limit: 1000,
-    //               filter: { frontmatter: { published: { ne: false } } }
-    //               sort: { order: DESC, fields: [frontmatter___date] }
-    //             ) {
-    //               edges {
-    //                 node {
-    //                   excerpt(pruneLength: 250)
-    //                   html
-    //                   fields {
-    //                     slug
-    //                     date
-    //                   }
-    //                   frontmatter {
-    //                     title
-    //                   }
-    //                 }
-    //               }
-    //             }
-    //           }
-    //         `,
-    //         output: '/rss.xml',
-    //         title: 'Blog RSS Feed',
-    //       },
-    //     ],
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.fields.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMdx(
+                  limit: 1000,
+                  filter: { frontmatter: { published: { ne: false } } }
+                  sort: { order: DESC, fields: [frontmatter___date] }
+                ) {
+                  edges {
+                    node {
+                      excerpt(pruneLength: 250)
+                      html
+                      fields {
+                        slug
+                        date
+                      }
+                      frontmatter {
+                        title
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml',
+            title: 'Blog RSS Feed',
+          },
+        ],
+      },
+    },
     'gatsby-plugin-offline',
   ],
 }
